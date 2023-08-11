35ae4fb9773e115d3450d9ac25cac999ddcb9924
edik <i.edik2001@gmail.com>
2023-08-11 12:09:38 +0300

    Файл, с настройками игры

---
+++ settings.py
@@ -0,0 +1,15 @@
+"""
+Файл с настройками игры.
+"""
+
+import pygame as pg
+
+WIDTH = 1280
+HEIGTH = 720
+FPS = 60
+COLOR_BACKGROUND = (30, 30, 30)
+WHITE = (255, 255, 255)
+pg.init()
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.SysFont('Corbel', 40)
